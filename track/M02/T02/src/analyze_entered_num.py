dd4f7b8c9833c00652a23ad3c2ae433be80f3fac
MythriReddy510 <my3gr2005@gmail.com>
2026-08-06 16:31:21 +0530

    Day1changes

---
+++ track/M02/T02/src/analyze_entered_num.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+positive_count = 0
+negative_count = 0
+zero_count = 0
+total = 0
+
+for i in range(1, n+1):
+    num = int(input())
+    total += num
+    if num > 0:
+        positive_count += 1
+    elif num < 0:
+        negative_count += 1
+    else:
+        zero_count += 1
+
+print(f"Positive Count: {positive_count}")
+print(f"Negative Count: {negative_count}")
+print(f"Zero Count: {zero_count}")
+print(f"Total: {total}")
