49176f09ca8a12530516f0356a86bffcd0c035eb
MythriReddy510 <my3gr2005@gmail.com>
2026-08-07 16:27:22 +0530

    Day1changes

---
+++ track/M02/T02/src/03_updae_tuple_without_modify.py
@@ -0,0 +1,10 @@
+course_name = input()
+cur_week = input()
+course_status = input()
+
+course_details = (course_name, cur_week, course_status)
+
+updated_week = input()
+course_details = (course_name, updated_week, course_status)
+
+print(course_details)
