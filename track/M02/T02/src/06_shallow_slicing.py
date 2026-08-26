ec43f53e03250bdeb7425c01ab142dfada77e639
MythriReddy510 <my3gr2005@gmail.com>
2026-08-26 14:24:25 +0530

    Day1changes

---
+++ track/M02/T02/src/06_shallow_slicing.py
@@ -0,0 +1,16 @@
+skills = []
+
+
+for _ in range(5):
+    skill = input()
+    skills.append(skill)
+
+
+skill_record = tuple(skills)
+
+
+print("Skill Record:", skill_record)
+print("First Three:", skill_record[:3])
+print("Last Two:", skill_record[-2:])
+print("Alternate Skills:", skill_record[::2])
+print("Reversed Skills:", skill_record[::-1])
